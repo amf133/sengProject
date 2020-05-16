7cf390fc3b7d2513a2e967326547e9c2c9b41cb6
Christian <Christian@ChristianPC>
2020-05-16 14:27:57 +1200

    animal store

--- farmProject/src/farmProject/W_AnimalStore.java
+++ farmProject/src/farmProject/W_AnimalStore.java
@@ -9,6 +9,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -26,6 +28,7 @@ import javax.swing.text.BadLocationException;
 import javax.swing.JTextArea;
 import java.awt.Color;
 import java.awt.SystemColor;
+import javax.swing.JComboBox;
 
 public class W_AnimalStore {
 
@@ -59,20 +62,21 @@ public class W_AnimalStore {
 		closeWindow();
 		manager.toTownMap();
 	}
-	/**
-	 * 
-	public void updateBal() {
-		lblMoney.setText("Current Balance: $" + manager.farmObject.getBal());
+	
+	public Map<String, Double> getAnimals(){
+		Map<String, Double> animals = new HashMap<>();
+		animals.put("Cow", 120.0);
+		animals.put("Pig", 150.0);
+		animals.put("Sheep", 100.0);
+		return animals;
 	}
 	
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize(Farm farmObject) {
 		
 		
 		frmAnimalStore = new JFrame();
 		frmAnimalStore.setTitle("Animal Store");
-		frmAnimalStore.setBounds(100, 100, 450, 300);
+		frmAnimalStore.setBounds(100, 100, 471, 319);
 		frmAnimalStore.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmAnimalStore.getContentPane().setLayout(null);
 		
@@ -90,11 +94,7 @@ public class W_AnimalStore {
 		txtView.setEditable(false);
 		frmAnimalStore.getContentPane().add(txtView);
 
-		
-		
 		JLabel lblMoney = new JLabel();
-		//BigDecimal Bal = new BigDecimal(farmObject.getBal());
-		//Bal = Bal.round(new MathContext(2));
 		lblMoney.setBounds(24, 21, 187, 14);
 		lblMoney.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblMoney.setText("Current Balance: $" + farmObject.getBal());
@@ -108,96 +108,53 @@ public class W_AnimalStore {
 				finishedWindow();
 			}
 		});
-		btnReturn.setBounds(49, 214, 91, 36);
+		btnReturn.setBounds(271, 233, 91, 36);
 		frmAnimalStore.getContentPane().add(btnReturn);
-		
-		// Button to buy cow
-		JButton btnBuyCow = new JButton("Cow $120");
-		btnBuyCow.setBackground(Color.CYAN);
-		double cow_cost = 120.0;
-		btnBuyCow.addMouseListener(new MouseAdapter(){
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if (farmObject.getBal() < cow_cost) {
-					showMessageDialog(null, "You do not have enough money!");
-				}
-				else {
-					Animal a = new Animal("Cow", cow_cost);
-					farmObject.addAnimal(a);
-					farmObject.updateBal(-cow_cost);
-					txtView.append("\n" + a.getType() + ": Happiness: " + a.getHappy() + " Health: " + a.getHealth());
-					lblMoney.setText(("Current Balance: $" + farmObject.getBal()));
-					
-					
-				}
-			}
-		});
-		btnBuyCow.setBounds(24, 92, 130, 36);
-		frmAnimalStore.getContentPane().add(btnBuyCow);
-		
-		// Button to buy pig
-		JButton btnBuyPig = new JButton("Pig $150");
-		btnBuyPig.setBackground(Color.PINK);
-		double pig_cost = 150.0;
-		btnBuyPig.addMouseListener(new MouseAdapter(){
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if (farmObject.getBal() < pig_cost) {
-					showMessageDialog(null, "You do not have enough money!");
-				}
-				else {
-					Animal a = new Animal("Pig", pig_cost);
-					manager.farmObject.addAnimal(a);
-					manager.farmObject.updateBal(-pig_cost);
-					lblMoney.setText(("Current Balance: $" + farmObject.getBal()));
-					txtView.append("\n" + a.getType() + ": Happiness: " + a.getHappy() + " Health: " + a.getHealth());
 
-				}
-			}
-		});
-		btnBuyPig.setBounds(24, 139, 130, 36);
-		frmAnimalStore.getContentPane().add(btnBuyPig);
+		JComboBox<String> comboBox = new JComboBox<>();
+		Map<String, Double> animal = getAnimals();
+		for (String a : animal.keySet()) {
+			comboBox.addItem(a + " $" + animal.get(a) + " ea");
+		}
+		comboBox.setBounds(24, 91, 130, 22);
+		comboBox.setEditable(false);
+		frmAnimalStore.getContentPane().add(comboBox);
+
 		
-		// Button to buy sheep
-		JButton btnBuySheep = new JButton("Sheep $100");
-		btnBuySheep.setBackground(Color.WHITE);
-		double sheep_cost = 100.0;
-		btnBuySheep.addMouseListener(new MouseAdapter(){
+		JButton btnConfirm = new JButton("Confirm");
+		btnConfirm.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				if (farmObject.getBal() < sheep_cost) {
-					showMessageDialog(null, "You do not have enough money!");
+				String arr[] = ((String) comboBox.getSelectedItem()).split(" ");
+				String type = arr[0];
+				Double cost = (Double) animal.get(type);
+				
+				if (farmObject.getAnimals().size() == farmObject.maxAnimals) {
+					showMessageDialog(null, "No space available");
+				}
+				else if (farmObject.getBal() < cost) {
+					showMessageDialog(null, "You do not have enough money");
 				}
 				else {
-					Animal a = new Animal("Sheep", sheep_cost);
+					Animal a = new Animal(type, cost);
 					farmObject.addAnimal(a);
-					farmObject.updateBal(-sheep_cost);
-					lblMoney.setText(("Current Balance: $" + farmObject.getBal()));
-					txtView.append("\n" + a.getType() + ": Happiness: " + a.getHappy() + " Health: " + a.getHealth());
+					farmObject.updateBal(-cost);
+					txtView.append("\n" + a.getType());
+					lblMoney.setText("Balance: " + farmObject.getBal());
 				}
+				
 			}
 		});
-		btnBuySheep.setBounds(24, 46, 130, 36);
-		frmAnimalStore.getContentPane().add(btnBuySheep);
+		btnConfirm.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnConfirm.setBounds(24, 124, 130, 23);
+		frmAnimalStore.getContentPane().add(btnConfirm);
+	
 		
+		JLabel lblSelect = new JLabel("Select Animal:");
+		lblSelect.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblSelect.setBounds(24, 66, 128, 14);
+		frmAnimalStore.getContentPane().add(lblSelect);
 		
-		// Testing transparent buttons
-		/**
-		btnBuyCow.setOpaque(false);
-		btnBuyCow.setContentAreaFilled(false);
-		btnBuySheep.setOpaque(false);
-		btnBuySheep.setContentAreaFilled(false);
-		btnBuyPig.setOpaque(false);
-		btnBuyPig.setContentAreaFilled(false);
-		
-		
-		
-		JLabel lblBackground = new JLabel("");
-		lblBackground.setVerticalAlignment(SwingConstants.TOP);
-		lblBackground.setIcon(new ImageIcon(TownMap.class.getResource("/Images/download.png")));
-		lblBackground.setBounds(0, 0, 890, 444);
-		frmAnimalStore.getContentPane().add(lblBackground);
-		*/
 
 	}
 }
